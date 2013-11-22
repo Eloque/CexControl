d3f1c367bf778b8fa53cda2c043c951edf3dc050
Eloque <mderuijter@DDY-MBP-002.local>
2013-11-22 17:45:48 +0100

    Initial
    Initial checkin of script

---
+++ CexControl.py
@@ -0,0 +1,234 @@
+#-------------------------------------------------------------------------------
+# Name:       CexControl
+# Purpose:    Automatically add mined coins on Cex.IO to GHS pool   
+#
+# Author:     Eloque
+#
+# Created:    19-11-2013
+# Copyright:  (c) Eloque 2013
+# Licence:    Free to use, copy and distribute as long as I'm credited
+#             Provided as is, use at your own risk and for your own benefit
+# Donate BTC: 17w7oe38d8Rm3pHYLwNZLn8TFSBVEaogJA     
+#-------------------------------------------------------------------------------
+
+import cexapi
+import re
+import time
+import json
+
+version = "0.3.2"
+
+NMCThreshold = 0.00010000
+BTCThreshold = 0.00010000
+
+def LoadSettings():
+
+    print "Loading Settings"
+    settings = json.load(open("CexControlSettings.conf"))
+
+    return settings
+
+def main():
+    
+    try:
+        settings = LoadSettings()
+    except:
+        print "Could not load settings, exiting"
+        exit()
+        
+    username    = str(settings['username'])
+    api_key     = str(settings['key'])
+    api_secret  = str(settings['secret'])
+
+    try:
+        context = cexapi.api(username, api_key, api_secret)
+        balance = context.balance()
+
+        print ("======= CexControl version %s =======") % version
+
+        print "Account       : %s" % username
+        print "GHS balance   : %s" % balance['GHS']['available']
+
+        print ("========================================")
+    
+    except:
+        print ("== !! ============================ !! ==")
+        print "Error:",
+        
+        try:
+            ErrorMessage = balance['error']
+        except:
+            ErrorMessage = "Unkown"
+        
+        print ErrorMessage
+
+        print ""
+
+        print "Could not connect Cex.IO, exiting"
+        print ("== !! ============================ !! ==")
+        exit()
+
+
+    while True:
+        now = time.asctime( time.localtime(time.time()) )
+        
+        try:
+       
+            print ""
+            print "%s" % now
+
+            balance = context.balance()
+            print "GHS balance: %s" % balance['GHS']['available']
+
+            CancelOrder(context)
+
+            CheckCoin("BTC", "GHS/BTC", context)
+            CheckCoin("NMC", "GHS/NMC", context)
+                
+        except:
+            print "== !! ============================ !! =="
+            print "Error:",
+        
+            try:
+                ErrorMessage = balance['error']
+            except:
+                ErrorMessage = "Unkown"
+        
+            print ErrorMessage
+
+            print "Could not connect Cex.IO, skipping cycle"
+            print "== !! ============================ !! =="
+            
+            
+        cycle = 300
+
+        while cycle > 0:
+            cycle = cycle - 1
+            time.sleep(1)
+
+
+    pass
+
+## Convert a unicode based float to a real float for us in calculations
+def ConvertUnicodeFloatToFloat( UnicodeFloat ):
+
+    ## I need to use a regular expression
+    ## get all the character from after the dot
+    position = re.search( '\.', UnicodeFloat)
+    if ( position ):
+        first = position.regs
+        place = first[0]
+        p = place[0]
+        p = p + 1
+
+        MostSignificant  = float(UnicodeFloat[:p-1])
+        LeastSignificant = float(UnicodeFloat[p:])
+
+        Factor = len(UnicodeFloat[p:])
+        Divider = 10 ** Factor
+
+        LeastSignificant = LeastSignificant / Divider
+
+        NewFloat = MostSignificant + LeastSignificant
+    else:
+        NewFloat = float(UnicodeFloat)
+
+
+    return NewFloat
+
+def CancelOrder(context):
+    ## BTC Order cancel
+    order = context.current_orders("GHS/BTC")
+    for item in order:
+        context.cancel_order(item['id'])
+        print "BTC Order %s canceled" % item['id']
+
+
+    ## NMC Order cancel
+    order = context.current_orders("GHS/NMC")
+    for item in order:
+        context.cancel_order(item['id'])
+        print "NMC Order %s canceled" % item['id']
+
+def CheckCoin( CoinName, TickerName, Context):
+    ## Get Balance of BTC
+    balance = Context.balance()
+
+    ## print balance
+
+    ## PlaceHolder
+    Threshold = BTCThreshold
+
+    Coin =  balance[CoinName]
+    Saldo = ConvertUnicodeFloatToFloat(Coin["available"])
+
+    ##print "Current Coin  :", CoinName
+    ##print "Current %s balance: %f" % CoinName, Coin["available"]
+    print "%s" % CoinName,
+    print "balance: %.8f" % Saldo
+
+    ## print "Convert saldo : %.8f" % Saldo
+    ## print "    threshold : %.8f" % Threshold
+
+
+    ## Do we need to buy GHS with BTC?
+    if ( Saldo > Threshold ):
+        Difference = Saldo-Threshold
+        ## print "       difference : %.8f" % Difference
+
+        print ("----------------------------------------")
+        ## print ("Buying threshold reached")
+
+        ## Get price
+        ticker = Context.ticker(TickerName)
+        ##print "--------------------------"
+        ##print ticker
+        ##print "--------------------------"
+
+        Ask = ConvertUnicodeFloatToFloat(ticker["ask"])
+        Bid = ConvertUnicodeFloatToFloat(ticker["bid"])
+        Price = (Ask+Bid) / 2
+        Price = round(Price,8)
+
+        AmountToBuy = Saldo / Price
+        AmountToBuy = round(AmountToBuy,8)
+
+        print "Price per GHS"
+        print "   Ask: %s" % ticker["ask"]
+        print "   Bid: %s" % ticker["bid"]
+        print " Price: %.8f" % Price
+        print "To buy: %.8f" % AmountToBuy
+
+        ## This is an HACK
+        Total = AmountToBuy * Price
+
+        while ( Total > Saldo ):
+            AmountToBuy = AmountToBuy - 0.0000005
+            AmountToBuy = round(AmountToBuy,8)
+
+            print ""
+            print "To buy adjusted to : %.8f" % AmountToBuy
+            Total = AmountToBuy * Price
+
+        result = Context.place_order('buy', AmountToBuy, Price, TickerName )
+
+        print ""
+        print "Placed order"
+        print "   Buy %.8f" % AmountToBuy,
+        print "at %.10f" % Price
+        print " Total %.8f" % Total
+        print " Funds %.8f" % Saldo
+
+        try:
+            OrderID = result['id']
+            print " Order ID", OrderID
+        except:
+            print result
+
+        print ("----------------------------------------")
+
+
+
+if __name__ == '__main__':
+    main()
+
